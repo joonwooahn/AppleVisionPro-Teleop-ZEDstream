bca0f1a6ca9029c584315381de0718d2bbac14c7
joonwooahn <joonwooahn@snu.ac.kr>
2025-09-17 02:20:37 +0000

    Fix H.264 server: remove unsupported tune parameter

--- avp_stream/zed_h264_tcp_server.sh
+++ avp_stream/zed_h264_tcp_server.sh
@@ -10,7 +10,7 @@ PORT="${1:-5000}"
 
 PIPELINE="v4l2src device=/dev/video0 ! video/x-raw,format=YUY2,width=1280,height=720,framerate=30/1 ! \
     videoconvert ! video/x-raw,format=NV12 ! \
-    nvv4l2h264enc maxperf-enable=1 insert-sps-pps=1 iframeinterval=30 bitrate=6000000 control-rate=1 preset-level=1 tune=low-latency ! \
+    nvv4l2h264enc maxperf-enable=1 insert-sps-pps=1 iframeinterval=30 bitrate=6000000 control-rate=1 preset-level=1 ! \
     h264parse config-interval=1 alignment=au ! video/x-h264,stream-format=byte-stream,alignment=au ! \
     tcpserversink host=0.0.0.0 port=${PORT}"
 
