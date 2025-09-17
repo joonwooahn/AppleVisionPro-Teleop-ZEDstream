72dac376b4a19a915691f96ebf4d9dd6a31801e7
joonwooahn <joonwooahn@snu.ac.kr>
2025-09-17 02:23:11 +0000

    Fix H.264 server: change format from NV12 to I420

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='avp_stream',
     version='1.0',
-    description='This python package streams diverse tracking data available from AVP to any devices that can communicate with gRPC.',
-    author='Younghyo Park',
-    author_email='younghyo@mit.edu',
+    description='This python package streams diverse tracking data available from AVP to any devices that can communicate with gRPC. Also streams ZED camera stream over TCP.',
+    author='Younghyo Park, Joonwoo Ahn',
+    author_email='younghyo@mit.edu, joonwooahn@snu.ac.kr',
     packages=find_packages(),
     install_requires=[
         'numpy', 'grpcio', 'grpcio-tools', 'matplotlib'
